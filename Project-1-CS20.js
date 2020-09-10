1f3de08ce39581c794728a1c64027e0128512f67
s-sutton3 <63067467+s-sutton3@users.noreply.github.com>
2020-09-10 15:05:35 -0600

    Update Project-1-CS20.js

--- Project-1-CS20.js
+++ Project-1-CS20.js
@@ -10,131 +10,89 @@ cnv.height = 600;
 ctx.fillStyle = "rgb(100, 100, 100)";
 ctx.fillRect(80, 260, 460, 80);
 
-// Filled Triangle for Hair
-fillTriangle(180, 80, 140, 100, 220, 100)
-
-// Filled Triangle for Hair
-fillTriangle 
-ctx.beginPath();
-ctx.moveTo(340, 80);
-ctx.lineTo(300, 100);
-ctx.lineTo(380, 100);
-ctx.fill();
-
-// Outlined Triangle for Hair
-ctx.beginPath();
-ctx.moveTo(260, 80);
-ctx.lineTo(220, 100);
-ctx.lineTo(300, 100);
-ctx.closePath()
-ctx.stroke();
-
-// Outlined Triangle for Hair
-ctx.beginPath();
-ctx.moveTo(420, 80);
-ctx.lineTo(380, 100);
-ctx.lineTo(460, 100);
-ctx.closePath()
-ctx.stroke();
-
-// Filled Triangle for Neck
-ctx.beginPath();
-ctx.moveTo(300, 200);
-ctx.lineTo(220, 600);
-ctx.lineTo(380, 600);
-ctx.fill();
+// Filled & outlined Triangles for Hair, neck & background
+fillTriangle(180, 80, 140, 100, 220, 100);
+fillTriangle(340, 80, 300, 100, 380, 100);
+strokeTriangle(260, 80, 220, 100, 300, 100);
+strokeTriangle(420, 80, 380, 100, 460, 100);
+fillTriangle(300, 200, 220, 600, 380, 600);
+fillTriangle(0, 50, 50, 50, 50, 50);
 
 // Rectangle Head
 ctx.fillStyle = "rgb(180, 180, 180)";
 ctx.fillRect(100, 100, 400, 400);
 
-// Filled Circle for Left Eye Socket
-ctx.fillStyle = "rgb(235, 235, 235)";
-ctx.beginPath();
-ctx.arc(200, 250, 50, 0, 2 * Math.PI);
-ctx.fill();
-
-// Filled Circle for Right Eye Socket
-ctx.beginPath();
-ctx.arc(400, 250, 50, 0, 2 * Math.PI);
-ctx.fill();
+// Filled Circles for Eye Sockets
+ctx.fillStyle = "rgb(235, 235, 235)"
+fillCircle(200, 250, 50, 0, 2);
+fillCircle(400, 250, 50, 0, 2);
 
 // Rectangle for Mouth
 ctx.fillRect(200, 350, 200, 60);
 
-// Horizontal Line for Teeth
-ctx.strokeStyle = "rgb(180, 180, 180)";
-ctx.beginPath();
-ctx.moveTo(200, 380);
-ctx.lineTo(400, 380);
-ctx.stroke();
+// Lines for Teeth
+ctx.strokeStyle = "rgb(180, 180, 180)"
+lines(200, 380, 400, 380);
+lines(250, 350, 250, 410);
+lines(300, 350, 300, 410);
+lines(350, 350, 350, 410);
 
-// First Vertical Line for Teeth
-ctx.beginPath();
-ctx.moveTo(250, 350);
-ctx.lineTo(250, 410);
-ctx.stroke();
+// Outlined and Filled circles for eyes
+fillShadedCircle(200, 250, 30, 0, 2);
+fillShadedCircle(400, 250, 10, 0, 2);
+outlineCircle(200, 250, 50, 0, 2);
+outlineCircle(400, 250, 50, 0, 2);
 
-// Second Vertical Line for Teeth
-ctx.beginPath();
-ctx.moveTo(300, 350);
-ctx.lineTo(300, 410);
-ctx.stroke();
-
-// Third Vertical Line for Teeth
-ctx.beginPath();
-ctx.moveTo(350, 350);
-ctx.lineTo(350, 410);
-ctx.stroke();
-
-// Filled Circle for Left Eye
-ctx.fillStyle = "rgb(100, 100, 100)";
-ctx.beginPath();
-ctx.arc(200, 250, 30, 0, 2 * Math.PI);
-ctx.fill();
-
-// Filled Circle for Right Eye
-ctx.beginPath();
-ctx.arc(400, 250, 10, 0, 2 * Math.PI);
-ctx.fill();
-
-// Outlined Circle for Left Eye Socket
-ctx.strokeStyle = "rgb(100, 100, 100)";
-ctx.beginPath();
-ctx.arc(200, 250, 50, 0, 2 * Math.PI);
-ctx.stroke();
-
-// Outlined Circle for Right Eye Socket
-ctx.beginPath();
-ctx.arc(400, 250, 50, 0, 2 * Math.PI);
-ctx.stroke();
-
-// Line for Left Eyebrow
-ctx.lineWidth = 2;
-ctx.beginPath();
-ctx.moveTo(150, 180);
-ctx.lineTo(250, 180);
-ctx.stroke();
-
-// Line for Right Eyebrow
-ctx.beginPath();
-ctx.moveTo(350, 160);
-ctx.lineTo(450, 180);
-ctx.stroke();
+// Lines for Eyebrows
+lines(150, 180, 250, 180);
+lines(350, 160, 450, 180);
 
 // Outlined Triangle for Nose
-ctx.beginPath();
-ctx.moveTo(300, 280);
-ctx.lineTo(320, 320);
-ctx.lineTo(280, 320);
-ctx.closePath();
-ctx.stroke();
+strokeTriangle(300, 280, 320, 320, 280, 320)
 
 // DRAW FUNCTIONS
 function fillTriangle(x1, y1, x2, y2, x3, y3) {
+    // DRAW A FILLED TRIANGLE WITH VERTICES (x1, y1), (x2,y2), (x3, y3)
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.lineTo(x3, y3);
     ctx.fill();
+}
+function strokeTriangle(x1, y1, x2, y2 ,x3, y3) {
+    // DRAW AN OUTLINED TRIANGLE WITH VERTICES (x1, y1), (x2, y2), (x3, y3)
+    ctx.beginPath();
+ctx.moveTo(x1, y1);
+ctx.lineTo(x2, y2);
+ctx.lineTo(x3, y3);
+ctx.closePath()
+ctx.stroke();
+}
+function fillCircle(x1, y1, x2, y2, x3) {
+    // DRAW A FILLED CIRCLE
+    ctx.beginPath();
+ctx.arc(x1, y1, x2, y2, x3 * Math.PI);
+ctx.fill();
+}
+function fillShadedCircle(x1, y1, x2, y2, x3) {
+    // DRAW A CIRCLE WHICH IS DARK IN COLOUR
+ctx.fillStyle = "rgb(100, 100, 100)";
+    ctx.beginPath();
+ctx.arc(x1, y1, x2, y2, x3 * Math.PI);
+ctx.fill();
+}
+function outlineCircle(x1, y1, x2, y2, x3) {
+    // Circle outline
+ctx.strokeStyle = "rgb(100, 100, 100)";
+ctx.beginPath();
+ctx.arc(x1, y1, x2, y2, x3 * Math.PI);
+ctx.stroke();
+}
+function lines(x1, y1, x2, y2) {
+    // Any lines that are disconnected from other shapes
+    ctx.lineWidth = 2;
+ctx.beginPath();
+ctx.moveTo(x1, y1);
+ctx.lineTo(x2, y2);
+ctx.stroke();
 }
